9e4a333b2b04170847b9ec7ca0a4938aad66276d
Peter Moroz <ptr.moroz@gmail.com>
2017-10-16 12:43:40 +0300

    process launcher  waits on CV when command to execute will be added

--- process_launcher.cpp
+++ process_launcher.cpp
@@ -40,9 +40,13 @@ std::vector<std::string> split(std::string s, char d)
 	return r;
 }
 
+
 ProcessLauncher::ProcessLauncher()
 	: _running(false)
 	, _commands()
+	, _commands_mutex()
+	, _awake_cv()
+	, _awake_cv_mutex()
 	, _observers()
 {
 }
@@ -55,7 +59,17 @@ ProcessLauncher::~ProcessLauncher()
 void ProcessLauncher::add_command(const std::string& command) noexcept
 {
 	try {
-		_commands.emplace_back(command);
+		
+		{
+			std::lock_guard<std::mutex> lg(_commands_mutex);
+			_commands.emplace_back(command);
+		}
+		
+		{
+			std::lock_guard<std::mutex> lg(_awake_cv_mutex);
+			_awake_cv.notify_one();
+		}
+		
 	} catch (const std::exception& ex) {
 		std::cerr << "Exception: " << ex.what() << std::endl;
 	}
@@ -68,17 +82,16 @@ void ProcessLauncher::run() noexcept
 	while (_running)
 	{
 		cmd.clear();
-		if (!_commands.empty())
+
 		{
-			std::cout << "PID " << ::getpid() << ", fetch next command..." << std::endl;
-			cmd = _commands.front();
-			_commands.pop_front();
-		}
-		else
-		{
-			std::this_thread::sleep_for(std::chrono::seconds(10));
+			std::unique_lock<std::mutex> ul(_awake_cv_mutex);
+			_awake_cv.wait(ul, [this](){ return (!_running || !_commands.empty()); });
 		}
 		
+		if (!_running)
+			break;
+			
+		cmd = fetch_command();
 		if (!cmd.empty())
 		{
 			std::cout << "PID " << ::getpid() << " command '" << cmd << "'" << std::endl;
@@ -119,21 +132,42 @@ void ProcessLauncher::run() noexcept
 		}
 	}
 	
-	std::vector<std::string> arguments = split(cmd, ' ');
-	static constexpr std::size_t MAX_ARGS = 64;
-	char** args = new char*[arguments.size() + 1];	// Memory leak! Move variable to global scope and register cleanup function by means atexit()
-	//std::cout << " Arguments list: " << std::endl;
-	std::size_t idx = 0;
-	for (; idx < arguments.size() && idx < MAX_ARGS; idx++)
+	if (!cmd.empty())
 	{
-		//std::cout << arguments[idx] << std::endl;
-		args[idx] = const_cast<char*>(arguments[idx].c_str());
+		std::cout << " launcher is going to spawn child process, command: " << cmd << std::endl;
+		std::vector<std::string> arguments = split(cmd, ' ');
+		static constexpr std::size_t MAX_ARGS = 64;
+		char** args = new char*[arguments.size() + 1];	// Memory leak! Move variable to global scope and register cleanup function by means atexit()
+		//std::cout << " Arguments list: " << std::endl;
+		std::size_t idx = 0;
+		for (; idx < arguments.size() && idx < MAX_ARGS; idx++)
+		{
+			//std::cout << arguments[idx] << std::endl;
+			args[idx] = const_cast<char*>(arguments[idx].c_str());
+		}
+		args[idx] = NULL;
+		
+		if (::execvp(args[0], args) == -1)
+		{
+			std::cerr << "execvp() failed: " << ::strerror(errno) << std::endl;
+		}	
 	}
-	args[idx] = NULL;
 	
-	if (::execvp(args[0], args) == -1)
-	{
-		std::cerr << "execvp() failed: " << ::strerror(errno) << std::endl;
+}
+
+void ProcessLauncher::stop() noexcept
+{
+	try {
+		if (_running)
+		{
+			_running = false;
+			{
+				std::lock_guard<std::mutex> lg(_awake_cv_mutex);
+				_awake_cv.notify_one();
+			}
+		}
+	} catch (const std::exception& ex) {
+		std::cerr << "Exception when stop: " << ex.what() << std::endl;
 	}
 }
 
@@ -141,4 +175,17 @@ void ProcessLauncher::add_process_observer(ProcessObserver* process_observer)
 {
 	assert(process_observer != NULL);
 	_observers.push_back(process_observer);
+}
+
+
+std::string ProcessLauncher::fetch_command()
+{
+	std::string cmd;
+	std::lock_guard<std::mutex> lg(_commands_mutex);
+	if (!_commands.empty())
+	{
+		cmd = _commands.front();
+		_commands.pop_front();
+	}
+	return cmd;
 }
--- process_launcher.h
+++ process_launcher.h
@@ -1,7 +1,8 @@
 #pragma once
 
-
+#include <condition_variable>
 #include <list>
+#include <mutex>
 #include <string>
 
 class ProcessObserver;
@@ -20,10 +21,18 @@ public:
 	void add_command(const std::string& command) noexcept;
 	void run() noexcept;
 	
+	void stop() noexcept;
+	
 	void add_process_observer(ProcessObserver* process_observer);
+	
+private:
+	std::string fetch_command();
 		
 private:
 	bool _running;
 	std::list<std::string> _commands;
+	std::mutex _commands_mutex;
+	std::condition_variable _awake_cv;
+	std::mutex _awake_cv_mutex;
 	std::list<ProcessObserver*> _observers;
 };
